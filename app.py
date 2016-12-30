d0f0ae6f61d452bbd877e22889315f731abf3b54
TheSDK <quintus.stierstorfer@bragi.com>
2016-12-30 16:11:26 +0100

    log

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def processRequest(req):
         return {}
     yql_url = baseurl + urllib.urlencode(yql_query) + "&format=json"
     #yql_url = "https://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27Boston%27%29&format=json"
+    print("yql_url: " + yql_url)
     result = urllib.urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
